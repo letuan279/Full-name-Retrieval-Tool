beae3337ecdbc1cc60f5d68d403ec344eafc5038
letuan279 <latuan388@gmail.com>
2024-08-16 23:47:48 +0700

    fix path to student search page

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <script setup>
-import { ref, computed } from 'vue'
-import FullnameRetrievalTool from './pages/FullnameRetrievalTool.vue';
-import StudentSearch from './pages/StudentSearch.vue';
-import NotFound from './pages/NotFound.vue';
+import { ref, computed } from "vue";
+import FullnameRetrievalTool from "./pages/FullnameRetrievalTool.vue";
+import StudentSearch from "./pages/StudentSearch.vue";
+import NotFound from "./pages/NotFound.vue";
 
 const routes = {
-  '/': FullnameRetrievalTool,
-  '/full-name-retrieval': FullnameRetrievalTool,
-  '/student-search': StudentSearch
-}
+  "/": StudentSearch,
+  // '/full-name-retrieval': FullnameRetrievalTool,
+  // '/student-search': StudentSearch
+};
 
-const currentPath = ref(window.location.pathname)
+const currentPath = ref(window.location.pathname);
 
-window.addEventListener('popstate', () => {
-  currentPath.value = window.location.pathname
-})
+window.addEventListener("popstate", () => {
+  currentPath.value = window.location.pathname;
+});
 
 const currentView = computed(() => {
-  return routes[currentPath.value || '/'] || NotFound
-})
+  return routes[currentPath.value || "/"] || NotFound;
+});
 </script>
 
 <template>
